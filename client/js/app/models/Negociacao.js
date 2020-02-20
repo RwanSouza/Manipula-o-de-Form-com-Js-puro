33924318628ba0e639abe41d36dc46c322a5920b
Rwan Souza <rwan.souza@hotmail.com>
2020-02-19 21:17:06 -0300

    Otimizando o código com a classe helper

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -8,20 +8,20 @@ class Negociacao {
       Object.freeze(this)
     }
 
-    get Volume(){
+    get volume(){
 
       return this._quantidade * this.valor; // calcula o volume da negociação.
     }
 
-    get Data(){
-      return this._data;
+    get data(){
+      return new Date(this._data.getTime());
     }
 
-    get Quantidade(){
+    get quantidade(){
       return this._quantidade;
     }
 
-    get Valor(){
+    get valor(){
       return this._valor;
     }
   }
